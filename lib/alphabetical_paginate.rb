8e23c7658bd0049956ac153b45e872b701a8981f
lingz <lz781@nyu.edu>
2013-07-11 17:50:41 -0400

    first commit

---
+++ lib/alphabetical_paginate.rb
@@ -0,0 +1,7 @@
+require "alphabetical_paginate/version"
+
+module AlphabeticalPaginate
+  # Your code goes here...
+  class Engine < Rails::Engine
+  end
+end
---
+++ lib/alphabetical_paginate/version.rb
@@ -0,0 +1,3 @@
+module AlphabeticalPaginate
+  VERSION = "0.0.1"
+end
