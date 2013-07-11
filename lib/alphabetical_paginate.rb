ca1abd4f369330bf8fc0221dac9964619c3afe09
lingz <lz781@nyu.edu>
2013-07-11 18:10:40 -0400

    second release

--- lib/alphabetical_paginate.rb
+++ lib/alphabetical_paginate.rb
@@ -3,5 +3,8 @@ require "alphabetical_paginate/version"
 module AlphabeticalPaginate
   # Your code goes here...
   class Engine < Rails::Engine
+    initializer 'static_assets.load_static_assets' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+    end
   end
 end
