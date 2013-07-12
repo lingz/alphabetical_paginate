bad365753b15a52307d23289cfdfe92a2b5a3e90
lingz <lz781@nyu.edu>
2013-07-12 16:51:59 -0400

    added alpha paginate

--- lib/alphabetical_paginate/array.rb
+++ lib/alphabetical_paginate/array.rb
@@ -1,5 +1,25 @@
 class Array
-  def hello_world
-    puts "hello_world"
+  def alpha_paginate current_field = "first"
+    output = []
+    field_val = block_given? ? yield(self) : self.id.to_s
+    field_letter = field_val.force_encoding(Encoding::ASCII_8BIT)[0]
+    if current_field == first
+      current_field = this.sort[0].to_s[0]
+    end
+    case current_field
+      when /[a-z]/ 
+        self.each do |x|
+          output << self if field_letter == current_field
+        end
+      when "number" 
+        self.each do |x|
+          output << self if field_letter =~ /[0-9]/
+        end
+      when "other"
+        self.each do |x|
+          output << self if !field_letter =~ /[0-9a-z]/
+        end
+    end
+    output
   end
 end
