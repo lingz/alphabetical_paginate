92a6e5d04ae437021b3644bfde98caac6eb0b7be
lingz <lz781@nyu.edu>
2013-07-12 12:35:13 -0400

    testing arrays

--- lib/alphabetical_paginate.rb
+++ lib/alphabetical_paginate.rb
@@ -1,4 +1,5 @@
 require "alphabetical_paginate/version"
+require "alphabetical_paginate/array"
 
 module AlphabeticalPaginate
   require 'alphabetical_paginate/engine'
---
+++ lib/alphabetical_paginate/array.rb
@@ -0,0 +1,5 @@
+class Array
+  def hello_world
+    puts "hello_world"
+  end
+end
