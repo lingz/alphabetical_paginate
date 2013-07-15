7c3caba0d4d872d13e2519fc26b2d60882d4c96b
lingz <lz781@nyu.edu>
2013-07-15 19:38:37 -0400

    added bootstrap pagination to view helper

--- lib/alphabetical_paginate/view_helpers.rb
+++ lib/alphabetical_paginate/view_helpers.rb
@@ -1,7 +1,19 @@
 module AlphabeticalPaginate
   module ViewHelpers
-    def alphabetical_paginate
+    def alphabetical_paginate params, bootstrap=true
       output = javascript_include_tag 'alphabetical_paginate' + " \n"
+      
+      params[:availableLetters].each do |l|
+        links += '<li><a href="#" data-letter="' + l + '">' + l + "</a></li>\n"
+
+      end
+      pagination = '<div class="pagination">\n' +
+        "<ul>\n" +
+        "%s" % links +
+        "</ul\n" +
+        "</div>"
+
+      output += pagination
     end
   end
 end
