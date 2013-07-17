6c3cc44e040f0b4b987f8557e30005c95fe1fe42
Oleg Grishin <og402@nyu.edu>
2013-07-17 17:40:58 +0400

    jQuery fix

--- vendor/assets/javascripts/alphabetical_paginate.js
+++ vendor/assets/javascripts/alphabetical_paginate.js
@@ -2,9 +2,15 @@ $(function() {
   var text = "<span class='loading'>Loading...</span>"
 
   $(".pagination a").live("click", function(e) {
-      jQuery.getScript(location.href.replace("(letter=?)", "letter=" + this.data("letter")));
+      if (/letter/.test(location.href)){
+        jQuery.getScript(location.href.replace(/letter\=./, "letter=" + this.data("letter")));
+        history.pushState(null, document.title, location.href.replace(/letter\=./, "letter=" + this.data("letter")));
+      }
+      else {
+        jQuery.getScript(location.href + "?letter=" + this.data("letter"));
+        history.pushState(null, document.title, location.href + "?letter=" + this.data("letter"));
+      }
       jQuery(".pagination").html(text);
-      history.pushState(null, document.title, location.href.replace("(letter=?)", "letter=" + this.data("letter")));
       e.preventDefault();
   });
 
