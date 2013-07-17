1003bee7508f0b028f618034c6fa5097422d4606
Oleg Grishin <og402@nyu.edu>
2013-07-17 18:04:50 +0400

    Back to the roots with the JS

--- vendor/assets/javascripts/alphabetical_paginate.js
+++ vendor/assets/javascripts/alphabetical_paginate.js
@@ -1,22 +1,18 @@
-console.log('asd');
 $(function() {
-  var text = "<span class='loading'>Loading...</span>"
+    var img = "<img src='/images/loader.gif' class='loading'/>"
+    var text = "<span class='loading'>Page is loading...</span>"
 
-  $(".pagination a").on("click", function(e) {
-      e.preventDefault();
-      if (/letter/.test(location.href)){
-        jQuery.getScript(location.href.replace(/letter\=./, "letter=" + this.data("letter")));
-        history.pushState(null, document.title, location.href.replace(/letter\=./, "letter=" + this.data("letter")));
-      }
-      else {
-        jQuery.getScript(location.href + "?letter=" + this.data("letter"));
-        history.pushState(null, document.title, location.href + "?letter=" + this.data("letter"));
-      }
-      jQuery(".pagination").html(text);
-  });
+    $(".pagination a").live("click", function(e) {
+        //jQuery.setFragment({ "page" : jQuery.queryString(this.href).page })
+        jQuery.getScript(this.href);
+        jQuery(".pagination").html(text+img);
+        history.pushState(null, document.title, this.href);
+        e.preventDefault();
+    });
 
-  $(window).on("popstate", function() {
-      $.getScript(location.href);
-      $(".pagination").html(text);
-  });
+    $(window).bind("popstate", function() {
+        $.getScript(location.href);
+        $(".pagination").html(text+img);
+    });
+
 });
