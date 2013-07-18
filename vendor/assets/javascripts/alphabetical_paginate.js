915953f9949d2275d0dd0c22b2b3f420ce51cdbf
lingz <lz781@nyu.edu>
2013-07-18 17:46:06 -0400

    minor syntax errors

--- vendor/assets/javascripts/alphabetical_paginate.js
+++ vendor/assets/javascripts/alphabetical_paginate.js
@@ -1,45 +1,45 @@
 $(function() {
-    var once = false;
+  var once = false;
 
-    var img = "<img src='/images/loader.gif' class='loading'/>"
-    
-    $(document).on("click", ".pagination#alpha a", function(e) {
-        var url = location.href,
-            letter = $(this).data("letter");
-        if (/letter/.test(url)){
-          url = url.replace(/letter=?./,"letter=" + letter); 
+  var img = "<img src='/images/aloader.gif' class='loading'/>";
+  
+  $(document).on("click", ".pagination#alpha a", function(e) {
+      var url = location.href,
+          letter = $(this).data("letter");
+      if (/letter/.test(url)){
+        url = url.replace(/letter=?./,"letter=" + letter); 
+      }
+      else {
+        if (/\?/.test(url)) {
+          url += "&letter=" + letter;
         }
         else {
-          if (/\?/.test(url)) {
-            url += "&letter=" + letter
-          }
-          else {
-            url += "?letter=" + letter 
-          }
+          url += "?letter=" + letter;
         }
-        $(".pagination").html(img);
-        //$.load(url + " #pagination_table");
-        $.get(url, function(result) {
-          $(".pagination").html($(".pagination", result));
-          $("#pagination_table").html($("#pagination_table", result));
-        });
-        history.pushState(null, document.title, url);
-        e.preventDefault();
-    });
-
-
- // let navigate the browser throught the ajax history
-$(window).bind("popstate", function() {
-  if (once) {
-    $(".pagination").html(img);
-    $.get(location.href, function(result) {
-      $(".pagination").html($(".pagination", result));
+      }
+      $(".pagination").html(img);
+      //$.load(url + " #pagination_table");
+      $.get(url, function(result) {
+        $(".pagination").html($(".pagination", result));
         $("#pagination_table").html($("#pagination_table", result));
       });
-    } else {
-      once = true;
-    }
-});
+      history.pushState(null, document.title, url);
+      e.preventDefault();
+  });
+
+
+   // let navigate the browser throught the ajax history
+  $(window).bind("popstate", function() {
+    if (once) {
+      $(".pagination").html(img);
+      $.get(location.href, function(result) {
+        $(".pagination").html($(".pagination", result));
+          $("#pagination_table").html($("#pagination_table", result));
+        });
+      } else {
+        once = true;
+      }
+  });
 
 
 });
