b4f8c3ec2b7bd8ecc04a00c266573099bda63d18
dej611 <marco.liberati@hotmail.it>
2014-12-01 22:11:17 +0000

    Restored old js

--- vendor/assets/javascripts/alphabetical_paginate.js
+++ vendor/assets/javascripts/alphabetical_paginate.js
@@ -1,7 +1,6 @@
 $(function() {
   var once = false;
 
-  // This should get the Rails path really
   var img = "<img src='/assets/aloader.gif' class='loading'/>";
   // RAILS 3.0 USERS -> Please delete the above line and uncomment the bottom line
   // ensure that the image directory matches
@@ -11,25 +10,27 @@ $(function() {
   // Pick the handler list: just a quick check for the jQuery version (see here: http://bugs.jquery.com/ticket/10589)
   var handlers = navbar.data ? navbar.data('events') : jQuery._data(navbar[0], 'events');
 
-  // Prevent multiple binding!!!
   if (!handlers || -1 !== $.inArray(onNavbarClick, handlers.click)) {
       $(document).on("click", ".pagination.alpha a", onNavbarClick);
-      // Bind popstate only if the pagination is active: 
-      // is production this code is always sent with the assets pipeline!
-      bindPopState();
   }
 
   function onNavbarClick(e) {
       e.preventDefault();
-      var url = location.href,
-          letter = $(this).data("letter");
+      var url = location.href, 
+      letter = $(this).data("letter");
       if (/letter/.test(url)) {
           url = url.replace(/letter=[^&]*/, "letter=" + letter);
-      } else {
-          url = ((/\?/.test(url)) ? '&' : '?') + 'letter=' + letter;
+      } 
+      else {
+          if (/\?/.test(url)) {
+              url += "&letter=" + letter;
+          } 
+          else {
+              url += "?letter=" + letter;
+          }
       }
       $(".pagination").html(img);
-
+      //$.load(url + " #pagination_table");
       $.get(url, function(result) {
           $(".pagination").html($(".pagination", result).html());
           $("#pagination_table").html($("#pagination_table", result).html());
@@ -37,42 +38,18 @@ $(function() {
       history.pushState(null, document.title, url);
   }
 
-  function bindPopState(){
-
-    // Bind it only once!
+   // let navigate the browser throught the ajax history
+  $(window).bind("popstate", function() {
     if (once) {
-
-       // let navigate the browser throught the ajax history
-      $(window).bind("popstate", function (jEvent) {
-        // Before run a quick check on the URL
-        // If the user click on a link handled with JS that adds a # to the URL
-        // This code here load the previous pagination state
-        var previousURL = jEvent.originalEvent.state;
-        // Check that the history change is due to the back button
-        if(needToPaginate(previousURL, location.href)){
-          $(".pagination").html(img);
-          $.get(location.href, function(result) {
-            $(".pagination").html($(".pagination", result).html());
-              $("#pagination_table").html($("#pagination_table", result).html());
-            });
-        }
-      });
-
-    } else {
-      once = true;
-    }
-  }
-
-  function needToPaginate(prev, current){
-    var regexp = /letter=[^&]*/;
-    if((regexp).test(prev) && (regexp).test(current)){
-      // same letter means don't paginate
-      return prev.match(regexp)[0] !== current.match(regexp)[0];
-    } else {
-      // If only one of the two has a letter than paginate
-      return (regexp).test(prev) || (regexp).test(current);
-    }
-  }
+      $(".pagination").html(img);
+      $.get(location.href, function(result) {
+        $(".pagination").html($(".pagination", result).html());
+          $("#pagination_table").html($("#pagination_table", result).html());
+        });
+      } else {
+        once = true;
+      }
+  });
 
 
 });
